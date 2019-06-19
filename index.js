900c08bd9e95561cabeb75c71404c508342dbe52
Alex <38507729+afenrir@users.noreply.github.com>
2019-06-19 08:24:23 +0100

    Prep branch for rewrite.

--- index.js
+++
@@ -1,2 +0,0 @@
-require('./src/init');
-require('./src');
--- src/factomd.js
+++
@@ -1,9 +0,0 @@
-const { getConfig } = require('./configuration')
-
-const { factomdConfig } = getConfig()
-
-const { FactomCli } = require('factom');
-
-const cli = new FactomCli(factomdConfig);
-
-module.exports = cli;
