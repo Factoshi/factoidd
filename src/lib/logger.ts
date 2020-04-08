1dff27f5eec2251bf1a1dd5acd5a9a11768a2d68
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2020-04-08 16:19:58 +0100

    refactor: reformat timestamp

--- src/lib/logger.ts
+++ src/lib/logger.ts
@@ -3,8 +3,8 @@ import { createLogger, format } from 'winston';
 const { combine, timestamp, printf, colorize, errors } = format;
 
 // This is used to format error instances correctly.
-const print = printf(info => {
-    const log = `${info.timestamp} - ${info.level}: ${info.message}`;
+const print = printf((info) => {
+    const log = `[${info.timestamp}]: ${info.level}: ${info.message}`;
 
     return info.stack ? `${log}: ${info.stack}` : log;
 });
@@ -12,5 +12,5 @@ const print = printf(info => {
 // The transport is created when imported into bin to allow
 // log level to be handle by command line args
 export const logger = createLogger({
-    format: combine(timestamp(), colorize(), errors({ stack: true }), print)
+    format: combine(timestamp(), colorize(), errors({ stack: true }), print),
 });
