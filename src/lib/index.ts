cc1549aad30e09a13c8ad684a4f004030de31235
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2020-04-17 11:15:43 +0100

    fix: export initialiseDatabase

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,6 +1,6 @@
 export { Factom } from './factom';
 export { logger } from './logger';
-export { TransactionTable } from './db';
+export { TransactionTable, initialiseDatabase } from './db';
 export { saveNewTransaction, emitNewTransactions } from './transaction';
 export { batchUpdatePrice } from './price';
 export { batchUpdateIncome } from './bitcointax';
