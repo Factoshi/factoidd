01db30020269e9bea2ba03bc6a66e8f62f928765
Alex Carrithers <38507729+afenrir@users.noreply.github.com>
2020-04-19 09:57:40 +0100

    fix: make env path neutral of cwd

--- env/index.js
+++ env/index.js
@@ -1,9 +1,10 @@
+const { resolve } = require('path');
 const dotenv = require('dotenv');
 
 // Load environment variables.
 const envFile = process.env.ENV_FILE || 'develop';
 const result = dotenv.config({
-    path: `./env/${envFile}.env`,
+    path: resolve(__dirname, `${envFile}.env`),
 });
 if (result.error) {
     console.log('No env file.');
